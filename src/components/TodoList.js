4b65097880e6845d696990928921d0776002f839
Antonio Paiva - Men <antonioivo.3@gmail.com>
2021-12-30 09:02:47 -0300

    first

---
+++ src/components/TodoList.js
@@ -0,0 +1,69 @@
+import React,{useState} from 'react'
+import Todoform from './Todoform'
+import Todo from './Todo';
+
+function TodoList() {
+    const[todos, setTodos] = useState([]);
+
+    const addTodo = todo => {
+
+        if(!todo.text || /^\s*$/.test(todo.text)){
+            return;
+        }
+
+        const newTodos = [todo, ...todos];
+
+        setTodos(newTodos);
+        
+    };
+
+    const updateTodo = (todoId, newValue) =>{
+        if(!newValue.text || /^\s*$/.test(newValue.text)){
+            return;
+        }
+
+        setTodos(prev => prev.map(item =>(item.id == todoId ? newValue: item)));
+
+
+
+    }
+
+    const removeTodo = id => {
+        const removeArr = [...todos].filter(todo => todo.id !== id);
+
+        setTodos(removeArr);
+    };
+
+   
+
+    const completeTodo = id => {
+        let updateTodos = todos.map(todo => {
+            if(todo.id ===id){
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        });
+        setTodos(updateTodos);
+    };
+
+    const completedTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id ===id){
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        })
+        setTodos(updatedTodos);
+    }
+
+    return (
+        <div>
+            <h1>Qual o plano de Hoje</h1>
+            <Todoform onSubmit={addTodo} />
+            <Todo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo}
+            updateTodo={updateTodo}/>
+        </div>
+    )
+}
+
+export default TodoList
